636cbc592af36b0e6fb3c0ec4aad4ab6ae5ac5d4
Ali Nti <Ali3nti@gmail.com>
2024-01-24 17:03:06 +0330

    add center widget

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("hello"),
+      body: Center(child: Text("hello")),
     );
   }
 }
