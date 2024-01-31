47301ec0c169bbb809cddf1d35b8884dd639219d
Ali Nti <Ali3nti@gmail.com>
2024-01-31 18:41:14 +0330

    change app bar (Ali)

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -56,185 +56,203 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white10,
-          title: Container(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white10,
+        scrolledUnderElevation: 0,
+        title: GestureDetector(
+          onTap: () {
+            search(context);
+          },
+          child: Container(
             height: 50,
+            padding: EdgeInsets.all(8),
             decoration: BoxDecoration(
               color: Colors.grey.shade300,
               borderRadius: BorderRadius.circular(10),
             ),
             child: Row(
               children: [
-                IconButton(
-                  onPressed: () {
-                    search(context);
-                  },
-                  icon:
-                      Icon(Icons.search, size: 30, color: Colors.grey.shade600),
-                ),
+                Icon(Icons.search, size: 30, color: Colors.grey.shade600),
                 Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      search(context);
-                    },
-                    child: Row(
-                      children: [
-                        const Text("جستجو در"),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Image.asset(
-                          'assets/images/logo.png',
-                          color: Colors.green.shade500,
-                          width: 90,
-                        ),
-                      ],
-                    ),
+                  child: Row(
+                    children: [
+                      const Text("جستجو در"),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Image.asset(
+                        'assets/images/logo.png',
+                        color: Colors.green.shade500,
+                        width: 90,
+                      ),
+                    ],
                   ),
                 )
               ],
             ),
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.location_on_outlined,
-                      color: Colors.green.shade500,
-                      size: 35,
-                    ),
-                    const Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 80),
-                          child: Text(
-                            "آدرس انتخابی",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          "آباده،میدان آزادی،کوچه هفتم",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(width: 60),
-                    TextButton(
-                      onPressed: () {
-                        location(context);
-                      },
-                      child: const Text(
-                        "تغییر آدرس",
-                        style: TextStyle(color: Colors.blue, fontSize: 15),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Stack(
-                alignment: Alignment.bottomLeft,
-                children: [
-                  SizedBox(
-                    height: 150,
-                    child: PageView(
-                      controller: pageController,
-                      children: [
-                        const SliderImage(
-                            ImageUrl:
-                                "https://dkstatics-public.digikala.com/jet-public/5803dca9a4f7e81d5db0d90a82ab98d34af5dc8b_1695207312.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90/format,webp"),
-                        const SliderImage(
-                            ImageUrl:
-                                "https://dkstatics-public.digikala.com/jet-public/216954b9069c9c07b50e24f2f6fbd1c4628a46b6_1695206052.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90/format,webp"),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 60, bottom: 20),
-                    child: SmoothPageIndicator(
-                      controller: pageController,
-                      count: 2,
-                      effect: const WormEffect(
-                          activeDotColor: Colors.black,
-                          dotHeight: 7,
-                          dotWidth: 7),
-                    ),
-                  ),
-                ],
-              ),
-              Wrap(
-                children: [
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                  Avatar(
-                      image: Image.asset("assets/images/p1.png"),
-                      text: "سوپرمارکت"),
-                ],
-              )
-            ],
-          ),
-        ),
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-            canvasColor: Colors.green,
-            primaryColor: Colors.red,
-          ),
-          child: BottomNavigationBar(
-            items: items,
-            currentIndex: index,
-            onTap: (indexTap) {
-              index = indexTap;
-              setState(() {});
-            },
-          ),
-        ),
-        // bottomNavigationBar: Container(
-        //   color: Colors.green.shade500,
-        //   height: 50,
-        //   child: Row(
-        //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-        //     children: [
-        //       Circle(iconData: Icons.home_filled, text: "صفحه اصلی"),
-        //       Circle(
-        //           iconData: Icons.shopping_basket_outlined, text: "سبد خرید"),
-        //       Circle(iconData: Icons.person_outline, text: "حساب"),
-        //       Circle(iconData: Icons.menu, text: "منو"),
-        //     ],
-        //   ),
-        // ),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.location_on_outlined,
+                    color: Colors.green.shade500,
+                    size: 35,
+                  ),
+                  const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 80),
+                        child: Text(
+                          "آدرس انتخابی",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "آباده،میدان آزادی،کوچه هفتم",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 60),
+                  TextButton(
+                    onPressed: () {
+                      location(context);
+                    },
+                    child: const Text(
+                      "تغییر آدرس",
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                SizedBox(
+                  height: 150,
+                  child: PageView(
+                    controller: pageController,
+                    children: [
+                      const SliderImage(
+                          ImageUrl:
+                              "https://dkstatics-public.digikala.com/jet-public/5803dca9a4f7e81d5db0d90a82ab98d34af5dc8b_1695207312.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90/format,webp"),
+                      const SliderImage(
+                          ImageUrl:
+                              "https://dkstatics-public.digikala.com/jet-public/216954b9069c9c07b50e24f2f6fbd1c4628a46b6_1695206052.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90/format,webp"),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 60, bottom: 20),
+                  child: SmoothPageIndicator(
+                    controller: pageController,
+                    count: 2,
+                    effect: const WormEffect(
+                        activeDotColor: Colors.black,
+                        dotHeight: 7,
+                        dotWidth: 7),
+                  ),
+                ),
+              ],
+            ),
+            Wrap(
+              children: [
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+                Avatar(
+                    image: Image.asset("assets/images/p1.png"),
+                    text: "سوپرمارکت"),
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.green,
+          primaryColor: Colors.red,
+        ),
+        child: BottomNavigationBar(
+          items: items,
+          currentIndex: index,
+          onTap: (indexTap) {
+            index = indexTap;
+            setState(() {});
+          },
+        ),
+      ),
+      // bottomNavigationBar: Container(
+      //   color: Colors.green.shade500,
+      //   height: 50,
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //     children: [
+      //       Circle(iconData: Icons.home_filled, text: "صفحه اصلی"),
+      //       Circle(
+      //           iconData: Icons.shopping_basket_outlined, text: "سبد خرید"),
+      //       Circle(iconData: Icons.person_outline, text: "حساب"),
+      //       Circle(iconData: Icons.menu, text: "منو"),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
