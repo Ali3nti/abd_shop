dbd46e41212f09123de5871fdd555069d16bdb89
mehdi <mehdidehghani556@yahoo.com>
2024-02-18 21:19:00 +0330

    create third page for cart(سبد خرید)

---
+++ lib/screens/cart/cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CartBody extends StatefulWidget {
+  const CartBody({super.key});
+
+  @override
+  State<CartBody> createState() => _CartBodyState();
+}
+
+class _CartBodyState extends State<CartBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.red,);
+  }
+}
