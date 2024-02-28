1935cdfe5bd42655e83cb883e7a906365de76d10
mehdi <mehdidehghani556@yahoo.com>
2024-02-28 12:40:13 +0330

    Edit Profile Page...

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -10,6 +10,59 @@ class ProfileBody extends StatefulWidget {
 class _ProfileBodyState extends State<ProfileBody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.blue,);
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 240),
+            child: Text(
+              "حساب کاربری",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Row(
+              children: [
+                Text(
+                  "محمدمهدی دهقانی",
+                  style: TextStyle(fontSize: 28),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.edit, color: Colors.blue, size: 30),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.notifications_rounded,
+                    size: 30,
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.output_outlined, size: 30),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 288),
+            child: Text(
+              "09164438875",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
