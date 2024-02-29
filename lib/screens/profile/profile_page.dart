5c25cb04f99e19a28df34d0705ea07657951b275
mehdi <mehdidehghani556@yahoo.com>
2024-02-29 13:34:19 +0330

    Add List tile at Profile Page

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:abd_shop/screens/profile/component/list_tile_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProfileBody extends StatefulWidget {
@@ -12,87 +13,164 @@ class _ProfileBodyState extends State<ProfileBody> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white70,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 240),
-            child: Text(
-              "حساب کاربری",
-              style: TextStyle(
-                fontSize: 30,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 240),
+              child: Text(
+                "حساب کاربری",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Row(
-              children: [
-                Text(
-                  "محمدمهدی دهقانی",
-                  style: TextStyle(fontSize: 28),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.edit, color: Colors.blue, size: 30),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.notifications_rounded,
-                    size: 30,
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "محمدمهدی دهقانی",
+                    style: TextStyle(fontSize: 28),
                   ),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.output_outlined, size: 30),
-                ),
-              ],
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit, color: Colors.blue, size: 30),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.notifications_rounded,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.output_outlined, size: 30),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 288),
-            child: Text(
-              "09164438875",
-              style: TextStyle(color: Colors.grey),
+            Padding(
+              padding: EdgeInsets.only(left: 288),
+              child: Text(
+                "09164438875",
+                style: TextStyle(color: Colors.grey),
+              ),
             ),
-          ),
-          Container(
-            width: 380,
-            height: 60,
-            decoration: BoxDecoration(
+            SizedBox(height: 100,),
+            Container(
+              width: 380,
+              height: 60,
+              decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: Colors.deepOrange, width: 3)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.headset_mic_rounded,
-                    color: Colors.deepOrange,
+                border: Border.all(color: Colors.deepOrange, width: 3),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.headset_mic_rounded,
+                      color: Colors.deepOrange,
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "نیاز به پشتیبانی دارم",
-                    style: TextStyle(color: Colors.deepOrange, fontSize: 20),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "نیاز به پشتیبانی دارم",
+                      style: TextStyle(color: Colors.deepOrange, fontSize: 20),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            ListTile(
+              title: Text("دعوت از دوستان"),
+              subtitle: Text("با دعوت از دوستاتون کد تخفیف رایگان بگیرید"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.people_outline),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text("آدرس ها"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.map_outlined),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text("کارت های هدیه"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.card_giftcard),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text("کدهای تخفیف"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.discount_outlined),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text("پیوستن به فروشگاه"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.shop_2_sharp),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text("پرسش های متداول"),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.question_answer_outlined),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 0.2,
+              color: Colors.grey,
+            ),
+          ],
+        ),
       ),
     );
   }
