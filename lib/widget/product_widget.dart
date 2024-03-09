baaefffddcda81ee67060b6c86ddc8a1f625c065
zahra kh <zahra.kh.dev@gmail.com>
2024-03-09 15:27:52 +0330

    change on product_widget

--- lib/widget/product_widget.dart
+++ lib/widget/product_widget.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class ProductWidget extends StatefulWidget {
   const ProductWidget({super.key});
@@ -23,132 +26,133 @@ class _ProductWidgetState extends State<ProductWidget> {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            alignment: Alignment.topRight,
-            child: Text(
-              "تخفیف کهکشانی", style: TextStyle(
-                color: Colors.orange,
-                fontSize: 18,
-                fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            width: 200,
-            height: 235,
-            //color: Colors.yellow,
-            child: Image.asset("assets/images/egg.webp", fit: BoxFit.contain),
-          ),
-          SizedBox(height: 20),
-          Row(
-            children: [
-              Container(
-                margin: EdgeInsets.only(right: 20),
-                decoration: BoxDecoration(
-                  //color: Colors.red,
-                    borderRadius: BorderRadius.circular(100)),
-                child: Image.asset(
-                  "assets/images/egg.webp",
-                  width: 30,
-                  //fit: BoxFit.contain
-                ),
-              ),
-              SizedBox(width: 10),
-              Text(
-                "اسم فروشگاه / آدرس",
-                style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          Container(
-            //color: Colors.green,
-            margin: EdgeInsets.only(left: 170),
-            child: Text(
-              "اسنک طلایی چی توز - 190 گرم ",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(height: 13),
-          Container(
-            width: double.infinity,
-            height: 70,
-            color: Colors.grey.shade100,
-            child: Row(
+          Expanded(
+            child: ListView(
               children: [
                 Container(
-                  margin: EdgeInsets.only(right: 20),
-                  width: 80,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50)),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "assets/images/egg.webp",
-                        width: 40,
-                        height: 30,
-                      ),
-                      SizedBox(width: 5),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 15,
-                      )
-                    ],
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    "تخفیف کهکشانی", style: TextStyle(
+                      color: Colors.orange,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(right: 20),
-                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
-                  width: 120,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50)),
-                  child: Row(
-                    children: [
-                      Text(
-                        "پفک و اسنک",
-                        style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(width: 3),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 15,
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            alignment: Alignment.topRight,
-            padding: EdgeInsets.only(right: 20),
-            //color: Colors.green,
-            child:Column(
-              children: [
-                Text("توضیحات",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
+                  width: 150,
+                  height: 200,
+                  child: Image.asset("assets/images/p1.png", fit: BoxFit.contain),
                 ),
                 SizedBox(height: 10),
-                Text("190 گرم"),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Image.asset(
+                        "assets/images/p11.png",
+                        width: 40,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "اسم فروشگاه / آدرس",
+                      style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: Text(
+                    "اسنک طلایی چی توز - 190 گرم ",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: 70,
+                  color: Colors.grey.shade100,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(right: 20),
+                        width: 70,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              "assets/images/p11.png",
+                              width: 40,
+                              height: 30,
+                            ),
+                            SizedBox(width: 5),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 20),
+                        padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
+                        width: 120,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Row(
+                          children: [
+                            Text(
+                              "پفک و اسنک",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 3),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 15,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+               SizedBox(height: 30),
+                Container(
+                  alignment: Alignment.topRight,
+                  padding: EdgeInsets.only(right: 20),
+                  child:Column(
+                    children: [
+                      Text("توضیحات", style: TextStyle(
+                          fontSize: 16,fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text("190 گرم", style: TextStyle(
+                        fontSize:14,),),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          SizedBox(height: 30),
           Container(
             color: Colors.grey.shade100,
-            height: 75,
+            height: 70,
             child: Row(
               children: [
                 Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  margin: EdgeInsets.only(right: 20),
                     alignment: Alignment.center,
                     width: 150,
                     height: 40,
@@ -159,13 +163,12 @@ class _ProductWidgetState extends State<ProductWidget> {
                     TextStyle(color: Colors.white,
                         fontSize: 14 , fontWeight: FontWeight.bold),)
                 ),
-                SizedBox(width: 70),
+                SizedBox(width: 110),
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 15),
+                  margin: EdgeInsets.only(top: 20),
                   child: Column(
                     children: [
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Container(
                             alignment: Alignment.center,
@@ -182,29 +185,32 @@ class _ProductWidgetState extends State<ProductWidget> {
                             ),
                           ),
                           SizedBox(width: 10),
-                          Text("35.350 تومان",
-                              style: TextStyle(fontSize: 14)),
-                        ],
-                      ),
+                          Container(
+                            child:
+                            Text("35,350",
+                                  style: TextStyle(fontSize: 14)),
+                                ),
+                                SizedBox(width: 5),
+                                Container(
+                                  child: Image.asset("assets/images/toman.png",
+                                    width: 15, height: 15,),
+                                ),
+                        ],  ),
                       Container(
-                        margin: EdgeInsets.only(right: 88),
-                        //color: Colors.white,
-                        child: Text(
-                          "50.500",
-                          style: TextStyle(
-                              decoration: TextDecoration.lineThrough,
-                              fontSize: 12,
-                              color: Colors.grey),
-                        ),
-                      ),
-                    ],
+                        margin: EdgeInsets.only(right: 70),
+                          child:Text("50.500", style: TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                            color: Colors.grey
+                      ),)
+    ),
+                        ],),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+                ],),
+                  ),
+              ]  ),
+            );
   }
 }
+
+
+
