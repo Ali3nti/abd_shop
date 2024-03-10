a0734fb30517a1dd1170c3c900ef9c388894abba
mehdi <mehdidehghani556@yahoo.com>
2024-03-10 14:06:16 +0330

    Edit & Update Container Widget

--- lib/screens/profile/component/container_widget.dart
+++ lib/screens/profile/component/container_widget.dart
@@ -1,42 +1,33 @@
 import 'package:flutter/material.dart';
 
-class ContainerWidget extends StatefulWidget {
-  const ContainerWidget({super.key, required BoxDecoration decoration, required int width, required int height, required Row child});
-
-  @override
-  State<ContainerWidget> createState() => _ContainerState();
-}
-
-class _ContainerState extends State<ContainerWidget> {
+class ContainerWidget extends StatelessWidget {
+  const ContainerWidget({super.key, required this.iconData, required this.text});
+   final IconData iconData;
+   final Text text;
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 380,
-      height: 60,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.deepOrange, width: 3),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(
+    width: 380,
+    height: 60,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(color: Colors.deepOrange, width: 3),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(color: Colors.deepOrange,
             onPressed: () {},
-            icon: Icon(
-              Icons.headset_mic_rounded,
-              color: Colors.deepOrange,
-            ),
-          ),
-          TextButton(
+            icon:Icon(iconData)
+
+        ),
+        TextButton(
             onPressed: () {},
-            child: Text(
-              "نیاز به پشتیبانی دارم",
-              style: TextStyle(color: Colors.deepOrange, fontSize: 20),
-            ),
-          ),
-        ],
-      ),
+            child:text
+        ),
+      ],
+    ),
     );
   }
 }
