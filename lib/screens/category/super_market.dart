e6c77cf3abca40d277a2a21054d8d15cc72150a7
mehdi <mehdidehghani556@yahoo.com>
2024-03-22 15:21:38 +0330

    Create Page for Super Market

---
+++ lib/screens/category/super_market.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SuperMarket extends StatefulWidget {
+  const SuperMarket({super.key});
+
+  @override
+  State<SuperMarket> createState() => _SuperMarketState();
+}
+
+class _SuperMarketState extends State<SuperMarket> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
