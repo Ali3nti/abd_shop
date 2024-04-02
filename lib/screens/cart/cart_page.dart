611532b012f69c76fe04d5ef57cf4f2288558271
zahra kh <zahra.kh.dev@gmail.com>
2024-04-02 15:52:41 +0330

    complete  cart_page

--- lib/screens/cart/cart_page.dart
+++ lib/screens/cart/cart_page.dart
@@ -17,131 +17,148 @@ class _CartBodyState extends State<CartBody> {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        title: Text("سبد خرید", style: TextStyle(fontSize: 28,
+        title: Text(
+          "سبد خرید",
+          style: TextStyle(
+            fontSize: 28,
             fontWeight: FontWeight.bold,
-          ),),
+          ),
+        ),
       ),
-      body: Container(
-        margin:EdgeInsets.only(top: 5),
-        color: Colors.white,
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(right: 10),
-                  //color: Colors.grey,
-                  width: 70,
-                  height: 70,
-                  child: Image.asset("assets/images/p8.png"),
-                ),
-                SizedBox(width: 10,),
-                Column(
-                  children: [
-                    Row(
+            Container(
+              margin: EdgeInsets.only(top: 5),
+              height: 200,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 10, right: 10),
+                        width: 60,
+                        height: 60,
+                        child: Image.asset("assets/images/p8.png"),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text("جت مارت", style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold),
+                              ),
+                              Text("|ونک", style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.bold),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(right: 190),
+                                width: 35,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade100,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Icon(Icons.navigate_next),
+                              ),
+                            ],),
+                          Container(
+                            margin: EdgeInsets.only(left: 160),
+                            child: Row(
+                              children: [
+                                Icon(Icons.bike_scooter_sharp, size: 15,),
+                                SizedBox(width: 5),
+                                Text("6,500 تومان", style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(width: 10),
+                                Text("45 دقیقه", style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,),
+                                ),
+                              ],),
+                          ),
+                        ],),
+                    ],),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Row(
                       children: [
-                        Text(
-                          "جت مارت", style: TextStyle(fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                        ),
-                        Text("|ونک", style: TextStyle(fontSize: 15,
-                            fontWeight: FontWeight.bold),
+                        Container(
+                          margin: EdgeInsets.only(right: 10),
+                          width: 50,
+                          height: 50,
+                          color: Colors.grey.shade50,
+                          child: Image.asset("assets/images/p16.png"),
                         ),
                         Container(
-                            margin: EdgeInsets.only(right: 180),
-                          width: 40,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade100,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                            child: Icon(Icons.navigate_next),
+                          margin: EdgeInsets.only(right: 1),
+                          width: 50,
+                          height: 50,
+                          color: Colors.grey.shade50,
+                          child: Image.asset("assets/images/p12.png"),
                         ),
+                        Container(
+                          margin: EdgeInsets.only(right: 1, left: 10),
+                          width: 50,
+                          height: 50,
+                          color: Colors.grey.shade50,
+                          child: Image.asset("assets/images/p7.png"),
+                        ),
+                        Column(
+                          children: [
+                            Icon(Icons.delete),
+                            Text("حذف سبد خرید", style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              color: Colors.grey.shade600,),
+                            ),
+                          ],),
                       ],),
-                    SizedBox(width: 50,),
-                    Row(
-                      children: [
-                        Icon(Icons.bike_scooter_sharp),
-                        SizedBox(width: 5,),
-                        Text("6.500 تومان", style: TextStyle(fontSize: 13,
-                            fontWeight: FontWeight.bold
-                          ),),
-                        SizedBox(width: 10,),
-                        Text("45 دقیقه", style: TextStyle(
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                      ],),
-                  ],),
-              ],),
-            Row(
-              children: [
-               Container(
-                 margin: EdgeInsets.only(right: 10),
-                 width: 60,
-                 height: 60,
-                 color: Colors.grey.shade50,
-                 child: Image.asset("assets/images/p16.png"),
-               ),
-                Container(
-                  margin: EdgeInsets.only(right: 1),
-                  width: 60,
-                  height: 60,
-                  color: Colors.grey.shade50,
-                  child: Image.asset("assets/images/p12.png"),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 1,left: 10),
-                  width: 60,
-                  height: 60,
-                  color: Colors.grey.shade50,
-                  child: Image.asset("assets/images/p7.png"),
-                ),
-                Column(
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
                     children: [
-                      Icon(Icons.delete),
-                      Text("حذف سبد خرید",style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey.shade600,
-                      ),),
+                      Container(
+                          margin: EdgeInsets.only(right: 20),
+                          width: 170,
+                          height: 40,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange.shade900,
+                              shape: ContinuousRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text("ادامه خرید", style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,),
+                            ),
+                          )),
+                      SizedBox(width: 70,),
+                      Column(
+                        children: [
+                          Text("جمع سبد خرید (3 مورد)", style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 20),
+                            child: Text("155.300 تومان", style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],),
                     ],),
-              ],),
-            SizedBox(height: 20,),
-            Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(right: 20),
-                    width: 170,
-                    height: 40,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange.shade900,
-                        shape: ContinuousRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        "ادامه خرید",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )),
-               SizedBox(width: 70,),
-                Column(children: [
-                  Text("جمع سبد خرید (3مورد)",style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey.shade500,
-                  ),),
-                  Text("155.300 تومان",style: TextStyle(
-                    fontSize: 14,fontWeight: FontWeight.bold,
-                  ),),
                 ],),
-              ],),
+            ),
           ],),
       ),
     );
