628cf22852b5e575c6d860de958e53a6777f2770
zahra kh <zahra.kh.dev@gmail.com>
2024-04-06 16:44:56 +0330

    add Each product_page

--- lib/screens/category/fruits_category.dart
+++ lib/screens/category/fruits_category.dart
@@ -1,4 +1,7 @@
+import 'package:abd_shop/widget/price_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FruitsCategory extends StatefulWidget {
   const FruitsCategory({super.key});
@@ -10,6 +13,140 @@ class FruitsCategory extends StatefulWidget {
 class _SuperMarketState extends State<FruitsCategory> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var market;
+    return Scaffold(
+      backgroundColor: Colors.grey.shade50,
+      appBar: AppBar(
+        actions: [
+          Icon(Icons.arrow_back),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            color: Colors.white,
+            height: 100,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Image.asset("assets/images/p7.png"),
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "هایپر میوه | ولیعصر",
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(right: 5),
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 5),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                  child: const Text(
+                                    "50%",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "میوه و سبزیجات",
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellow.shade700,
+                                  size: 18,
+                                ),
+                                const SizedBox(width: 7),
+                                Text(
+                                  "4.3",
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(width: 7),
+                                const Icon(Icons.delivery_dining),
+                                const SizedBox(width: 7),
+                                const PriceWidget(price: 8500)
+                              ],
+                            ),
+                            const Text(
+                              "ارسال رایگان برای خرید بالای 20 تومان",
+                              style: TextStyle(
+                                color: Colors.orangeAccent,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        // margin: EdgeInsets.only(left: 50),
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "45",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "دقیقه",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
