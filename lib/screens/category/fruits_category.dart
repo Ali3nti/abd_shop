61b3d73c6ea58b80739266fdd82c1ed4bd179eaf
mehdi <mehdidehghani556@yahoo.com>
2024-04-06 11:06:13 +0330

    Create page for fruit category

---
+++ lib/screens/category/fruits_category.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FruitsCategory extends StatefulWidget {
+  const FruitsCategory({super.key});
+
+  @override
+  State<FruitsCategory> createState() => _SuperMarketState();
+}
+
+class _SuperMarketState extends State<FruitsCategory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
