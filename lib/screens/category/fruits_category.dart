0faa5a75e133cad083a31a8fcc46e50d58a4913b
zahra kh <zahra.kh.dev@gmail.com>
2024-04-07 15:07:34 +0330

    change each product_page

--- lib/screens/category/fruits_category.dart
+++ lib/screens/category/fruits_category.dart
@@ -1,6 +1,8 @@
+import 'package:abd_shop/screens/search/search_page.dart';
 import 'package:abd_shop/widget/price_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class FruitsCategory extends StatefulWidget {
@@ -8,7 +10,17 @@ class FruitsCategory extends StatefulWidget {
 
   @override
   State<FruitsCategory> createState() => _SuperMarketState();
-}
+
+  search(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SearchPage(),
+      ),
+    );
+  }
+
+  }
 
 class _SuperMarketState extends State<FruitsCategory> {
   @override
@@ -17,135 +29,180 @@ class _SuperMarketState extends State<FruitsCategory> {
     return Scaffold(
       backgroundColor: Colors.grey.shade50,
       appBar: AppBar(
-        actions: [
-          Icon(Icons.arrow_back),
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Container(
-            color: Colors.white,
-            height: 100,
+        backgroundColor: Colors.white10,
+        scrolledUnderElevation: 0,
+        title: GestureDetector(
+          onTap: () {
+          },
+          child: Container(
+            height: 50,
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30,
-                    child: Image.asset("assets/images/p7.png"),
-                  ),
+                Icon(Icons.search, size: 30, color: Colors.grey.shade600),
+                SizedBox(
+                  width: 5,
                 ),
                 Expanded(
                   child: Row(
                     children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
+                      const Text(
+                        "جستجو در",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Image.asset(
+                        'assets/images/logo.png',
+                        color: Colors.deepOrange,
+                        width: 90,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Container(
+              color: Colors.white,
+              height: 110,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10,right: 15),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 30,
+                      child: Image.asset("assets/images/p7.png"),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                     margin: EdgeInsets.only(top: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  "هایپر میوه | ولیعصر",
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(right: 5),
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 5),
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.circular(50),
-                                  ),
-                                  child: const Text(
-                                    "50%",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.white,
+                                Row(
+                                  children: [
+                                    Text(
+                                      "هایپر میوه سایه | ولیعصر",
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
+                                    Container(
+                                      margin: const EdgeInsets.only(right: 5),
+                                      padding:
+                                          const EdgeInsets.symmetric(horizontal: 5),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      child: const Text(
+                                        "18%",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "میوه و سبزیجات",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey.shade500,
                                   ),
                                 ),
-                              ],
-                            ),
-                            Text(
-                              "میوه و سبزیجات",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey.shade500,
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.yellow.shade700,
-                                  size: 18,
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow.shade700,
+                                      size: 18,
+                                    ),
+                                    const SizedBox(width: 7),
+                                    Text(
+                                      "4.3",
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 7),
+                                    const Icon(Icons.delivery_dining),
+                                    const SizedBox(width: 7),
+                                    const PriceWidget(price: 7500,size: Size.large,)
+                                  ],
                                 ),
-                                const SizedBox(width: 7),
-                                Text(
-                                  "4.3",
-                                  style: const TextStyle(
+                                const Text(
+                                  "ارسال رایگان برای خرید بالای 280,000 تومان",
+                                  style: TextStyle(
+                                    color: Colors.orangeAccent,
                                     fontSize: 12,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                                const SizedBox(width: 7),
-                                const Icon(Icons.delivery_dining),
-                                const SizedBox(width: 7),
-                                const PriceWidget(price: 8500)
                               ],
                             ),
-                            const Text(
-                              "ارسال رایگان برای خرید بالای 20 تومان",
-                              style: TextStyle(
-                                color: Colors.orangeAccent,
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
+                          ),
+                          Container(
+                             margin: EdgeInsets.only(left: 10),
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade50,
+                              borderRadius: BorderRadius.circular(20),
                             ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        // margin: EdgeInsets.only(left: 50),
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "45",
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "45",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "دقیقه",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
                             ),
-                            Text(
-                              "دقیقه",
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
