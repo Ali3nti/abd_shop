55bcfe4a7278725b4a4513680aa17e43633f5746
mehdi <mehdidehghani556@yahoo.com>
2024-04-12 16:24:01 +0330

    Edit size at amazing item widget Becoming more like Digi jet

--- lib/screens/home/components/amazing_item_widget.dart
+++ lib/screens/home/components/amazing_item_widget.dart
@@ -10,135 +10,143 @@ class AmazingItemWidget extends StatefulWidget {
 class _AmazingItemWidgetState extends State<AmazingItemWidget> {
   @override
   Widget build(BuildContext context) {
-    return  Container(decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10)),
-        padding: const EdgeInsets.all(8),
+    return Padding(padding: EdgeInsets.only(top: 20,bottom: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        // padding: const EdgeInsets.all(8),
         height: 300,
         width: 180,
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  height: 110,
-                  child: Image.asset(
-                    "assets/images/p1.png",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: FloatingActionButton.extended(
-                    onPressed: () {
-                      //TODO: Create "Add to cart" function
-                    },
-                    backgroundColor: Colors.orange.shade900,
-                    foregroundColor: Colors.white,
-                    isExtended: true,
-                    shape: const CircleBorder(
-                      eccentricity: 0,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 110,
+                    child: Image.asset(
+                      "assets/images/p1.png",
+                      fit: BoxFit.contain,
                     ),
-                    label: const Row(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: FloatingActionButton.extended(
+                      onPressed: () {
+                        //TODO: Create "Add to cart" function
+                      },
+                      backgroundColor: Colors.orange.shade900,
+                      foregroundColor: Colors.white,
+                      isExtended: true,
+                      shape: const CircleBorder(
+                        eccentricity: 0,
+                      ),
+                      label: const Row(
+                        children: [
+                          Icon(Icons.add),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // Container(
+                  //   //margin: EdgeInsets.only(top: 50),
+                  //   width: 50,
+                  //   height: 50,
+                  //   decoration: BoxDecoration(
+                  //     color: Colors.orange.shade900,
+                  //     borderRadius: BorderRadius.circular(100),
+                  //   ),
+                  //   child: Icon(Icons.add, color: Colors.white, size: 30),
+                  // ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: const Text(
+                      "پنیر لبنه می ماس",
+                      style: TextStyle(
+                        fontSize: 20,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: const Text(
+                      "300 گرم",
+                      style: TextStyle(
+                        fontSize: 20,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Row(
                       children: [
-                        Icon(Icons.add),
+                        Row(
+                          children: [
+                            const Text(
+                              "37,500",
+                              style: TextStyle(
+                                fontSize: 18,
+                                // fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(width: 6),
+                            Image.asset(
+                              'assets/images/toman.png',
+                              scale: 5.0,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 12),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 2,
+                            horizontal: 8,
+                          ),
+                          // alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Text(
+                            "25 %",
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                ),
-                // Container(
-                //   //margin: EdgeInsets.only(top: 50),
-                //   width: 50,
-                //   height: 50,
-                //   decoration: BoxDecoration(
-                //     color: Colors.orange.shade900,
-                //     borderRadius: BorderRadius.circular(100),
-                //   ),
-                //   child: Icon(Icons.add, color: Colors.white, size: 30),
-                // ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: const Text(
-                    "پنیر لبنه می ماس",
-                    style: TextStyle(
-                      fontSize: 20,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: const Text(
-                    "300 گرم",
-                    style: TextStyle(
-                      fontSize: 20,
-                      // fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: Row(
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            "37,500",
-                            style: TextStyle(
-                              fontSize: 18,
-                              // fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(width: 6),
-                          Image.asset(
-                            'assets/images/toman.png',
-                            scale: 5.0,
-                          ),
-                        ],
+                  const SizedBox(height: 5),
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: const Text(
+                      "50,000",
+                      style: TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                        fontSize: 15,
+                        color: Colors.grey,
                       ),
-                      const SizedBox(width: 12),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 8,
-                        ),
-                        // alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Text(
-                          "25 %",
-                          style:
-                              TextStyle(fontSize: 18, color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: const Text(
-                    "50,000",
-                    style: TextStyle(
-                      decoration: TextDecoration.lineThrough,
-                      fontSize: 15,
-                      color: Colors.grey,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
