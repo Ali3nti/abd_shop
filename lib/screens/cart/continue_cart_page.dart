216a2eed251a6f74917bdc55ea2837848a7e58e8
zahra kh <zahra.kh.dev@gmail.com>
2024-06-14 17:39:27 +0330

    add_continue_cart_page

---
+++ lib/screens/cart/continue_cart_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ContinueCartPage extends StatefulWidget {
+  const ContinueCartPage({super.key});
+
+  @override
+  State<ContinueCartPage> createState() => _ContinueCartPageState();
+}
+
+class _ContinueCartPageState extends State<ContinueCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child:
+        Text("ContinueCartPage"),
+      ),
+    );
+  }
+}
