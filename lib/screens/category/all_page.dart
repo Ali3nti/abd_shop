6531b9b7e43dbc2712e587058e14908155b34779
zahra kh <zahra.kh.dev@gmail.com>
2024-06-14 17:38:38 +0330

    add_all_page

---
+++ lib/screens/category/all_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AllPage extends StatefulWidget {
+  const AllPage({super.key});
+
+  @override
+  State<AllPage> createState() => _AllPageState();
+}
+
+class _AllPageState extends State<AllPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("all page"),
+      ),
+    );
+  }
+}
