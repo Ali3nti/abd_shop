01ca7f0ce0a83ca54043df80427bb184725af4a6
mehdi <mehdidehghani556@yahoo.com>
2024-07-03 14:56:15 +0330

    Edit At slider page

--- lib/screens/slider_page/slider_page1.dart
+++ lib/screens/slider_page/slider_page1.dart
@@ -12,7 +12,8 @@ class SliderPage extends StatefulWidget {
 class _SliderPageState extends State<SliderPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.white,
       appBar: MyAppBar(),
       body: SingleChildScrollView(
         child: Column(
@@ -20,53 +21,130 @@ class _SliderPageState extends State<SliderPage> {
             Row(
               children: [
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
                   picture: "assets/images/p32.png",
-                  priceproduct: "35000",
+                  priceproduct: "35,000",
                   productdetail: "نوشابه انرژی زا گازدار ادج",
                 ),
-                SizedBox(width:11 ,),
+                SizedBox(
+                  width: 11,
+                ),
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
-                  picture: "assets/images/p32.png",
-                  priceproduct: "35000",
-                  productdetail: "نوشابه انرژی زا گازدار ادج",
+                  picture: "assets/images/p33.png",
+                  priceproduct: "20,000",
+                  productdetail: "نوشیدنی آّب آلبالو",
                 ),
               ],
+            ),
+            Container(
+              height: 2,
+              width:double.infinity,
+              color: Colors.grey.shade300,
             ),
             Row(
               children: [
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
-                  picture: "assets/images/p32.png",
-                  priceproduct: "35000",
-                  productdetail: "نوشابه انرژی زا گازدار ادج",
+                  picture: "assets/images/p34.png",
+                  priceproduct: "32,310",
+                  productdetail: "اسنک سرکه نمکی یامی",
                 ),
-                SizedBox(width:11 ,),
+                SizedBox(
+                  width: 11,
+                ),
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
-                  picture: "assets/images/p32.png",
-                  priceproduct: "35000",
-                  productdetail: "نوشابه انرژی زا گازدار ادج",
+                  picture: "assets/images/p35.png",
+                  priceproduct: "35,000",
+                  productdetail: "نوشابه انرژی‌زا گاز دار هایپ",
                 ),
               ],
+            ),
+            Container(
+              height: 2,
+              width:double.infinity,
+              color: Colors.grey.shade300,
             ),
             Row(
               children: [
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
-                  picture: "assets/images/p32.png",
-                  priceproduct: "35000",
-                  productdetail: "نوشابه انرژی زا گازدار ادج",
+                  picture: "assets/images/p36.png",
+                  priceproduct: "85,000",
+                  productdetail: "انرژی زا بلک ولف یک لیتری",
                 ),
-                SizedBox(width:11 ,),
+                SizedBox(
+                  width: 11,
+                ),
                 ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
                   onTap: () {},
-                  picture: "assets/images/p32.png",
-                  priceproduct: "35000",
-                  productdetail: "نوشابه انرژی زا گازدار ادج",
+                  picture: "assets/images/p37.png",
+                  priceproduct: "14,000",
+                  productdetail: "ماست موسیر چکیده میهن",
                 ),
               ],
+            ),
+            Container(
+              height: 2,
+              width:double.infinity,
+              color: Colors.grey.shade300,
+            ),
+            Row(
+              children: [
+                ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {},
+                  picture: "assets/images/p38.png",
+                  priceproduct: "20,000",
+                  productdetail: "استیک سیب زمینی کچاپ چی‌توز",
+                ),
+                SizedBox(
+                  width: 11,
+                ),
+                ProductWidgetTwo(
+                  percent: Text(
+                    "10%",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {},
+                  picture: "assets/images/p39.png",
+                  priceproduct: "78,000",
+                  productdetail: "آب انار و بلوبری سان‌استار",
+                ),
+              ],
+            ),
+            Container(
+              height: 2,
+              width:double.infinity,
+              color: Colors.grey.shade300,
             ),
           ],
         ),
