5b85a7b103497a9a4a13ef5ab6ce74b104263927
zahra kh <zahra.kh.dev@gmail.com>
2024-07-15 17:50:51 +0330

    change on internet_payment_page

--- lib/screens/cart/internet_payment.dart
+++ lib/screens/cart/internet_payment.dart
@@ -10,7 +10,7 @@ class InternetPayment extends StatefulWidget {
 class _InternetPaymentState extends State<InternetPayment> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text("پرداخت اینترنتی"),
       ),
