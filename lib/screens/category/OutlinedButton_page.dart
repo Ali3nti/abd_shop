d32cbae5486d79c325df488a708e7398b7af8dee
zahra kh <zahra.kh.dev@gmail.com>
2024-08-12 18:36:50 +0330

    outline page

---
+++ lib/screens/category/OutlinedButton_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OutLinedButtonPage extends StatefulWidget {
+  const OutLinedButtonPage({super.key});
+
+  @override
+  State<OutLinedButtonPage> createState() => _OutLinedButtonPageState();
+}
+
+class _OutLinedButtonPageState extends State<OutLinedButtonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("page"),
+      ),
+    );
+  }
+}
