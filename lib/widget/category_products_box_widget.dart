6ff2b368d6fe38b93130874eb0f8ebeb182bd144
zahra kh <zahra.kh.dev@gmail.com>
2024-10-15 15:52:28 +0330

    change_on_category_products_box_widget

--- lib/widget/category_products_box_widget.dart
+++ lib/widget/category_products_box_widget.dart
@@ -1,6 +1,6 @@
 import 'package:abd_shop/constants.dart';
 import 'package:abd_shop/screens/category/all_page.dart';
-import 'package:abd_shop/widget/discount_widget/discount_list_widget.dart';
+import 'package:abd_shop/screens/home/components/amazing_product/amazing_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class CategorProductyBox extends StatefulWidget {
@@ -50,14 +50,14 @@ class _CategorProductyBoxState extends State<CategorProductyBox> {
                 const Icon(Icons.navigate_next, color: Colors.blue),
               ],
             ),
-            SingleChildScrollView(
+            const SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
                   SizedBox(
                     width: 20,
                   ),
-                  DiscountListWidget(),
+                  AmazingListWidget(),
                   SizedBox(
                     width: 20,
                   ),
