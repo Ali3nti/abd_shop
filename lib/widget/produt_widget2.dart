423e8fff4c1edb872f952ade0918d19e0faf7e96
mehdi <mehdidehghani556@yahoo.com>
2024-10-19 12:52:45 +0330

    Fixed problem at product widget and edit sizedbox at homebody

--- lib/widget/produt_widget2.dart
+++ lib/widget/produt_widget2.dart
@@ -20,99 +20,89 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(margin: EdgeInsets.all(100),
+    return Card(shadowColor: Colors.cyanAccent,
       elevation: 20,
-      child: Container(
-        height: 300, // ارتفاع مشخص برای جلوگیری از overflow
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Image.asset(image, height: 100, width: 100),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Text(
-                        discountPercentage + '%',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              Image.asset(image, height: 100, width: 130),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Text(
+                      discountPercentage + '%',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Row(
                 children: [
                   Text(
-                    title,
+                    discountedPrice,
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 16,
+                      color: Colors.red,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        discountedPrice,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Text(
-                        price,
-                        style: TextStyle(
-                          fontSize: 14,
-                          decoration: TextDecoration.lineThrough,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
+                  SizedBox(width: 8),
                   Text(
-                    '${quantity} (${quantity.substring(0, 2)} عدد)',
+                    price,
                     style: TextStyle(
                       fontSize: 14,
+                      decoration: TextDecoration.lineThrough,
                       color: Colors.grey,
                     ),
                   ),
                 ],
               ),
-            ),
-            Spacer(), // اینجا برای ایجاد فضای خالی بین محتوا و دکمه
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton.extended(
-                  heroTag: "button",
-                  onPressed: () {},
-                  backgroundColor: Colors.orange.shade900,
-                  foregroundColor: Colors.white,
-                  isExtended: true,
-                  shape: const CircleBorder(
-                    eccentricity: 0,
-                  ),
-                  label: Icon(Icons.add),
+              Text(
+                '${quantity} (${quantity.substring(0, 2)} عدد)',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
                 ),
-              ],
+              ),
+            ],
+          ),
+          SizedBox(width: 40,
+            child: FloatingActionButton.extended(
+              heroTag: "button",
+              onPressed: () {},
+              backgroundColor: Colors.orange.shade900,
+              foregroundColor: Colors.white,
+              isExtended: true,
+              shape: const CircleBorder(
+                eccentricity: 0,
+              ),
+              label: Icon(Icons.add),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
