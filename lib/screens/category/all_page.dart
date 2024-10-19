7ecd55e78ae4b20c011b01a0c493f062b96f8c6b
zahra kh <zahra.kh.dev@gmail.com>
2024-10-19 22:11:49 +0330

    change_on_all_page

--- lib/screens/category/all_page.dart
+++ lib/screens/category/all_page.dart
@@ -1,8 +1,6 @@
 import 'package:abd_shop/screens/home/components/my_app_bar.dart';
 import 'package:abd_shop/widget/provider_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class AllPage extends StatefulWidget {
   const AllPage({super.key});
@@ -21,27 +19,27 @@ class _AllPageState extends State<AllPage> {
         child: Column(
           children: [
             Container(
-              margin: EdgeInsets.only(top: 20),
+              margin: const EdgeInsets.only(top: 20),
               width: double.infinity,
               height: 50,
               // color: Colors.red,
               child: Row(
                 children: [
-                  SizedBox(width: 20),
-                  Icon(
+                  const SizedBox(width: 20),
+                  const Icon(
                     Icons.line_weight,
                     color: Colors.black,
                     size: 18,
                   ),
-                  SizedBox(width: 20),
+                  const SizedBox(width: 20),
                   Container(
                     width: 100,
                     height: 40,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(5),
-                      color: Colors.grey,
+                      color: Colors.grey.shade200,
                     ),
-                    child: Center(
+                    child: const Center(
                       child: Text("همه محصولات"),
                     ),
                   ),
@@ -49,19 +47,19 @@ class _AllPageState extends State<AllPage> {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 10,right: 20),
-              margin: EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.only(top: 10,right: 20),
+              margin: const EdgeInsets.only(top: 20),
               width: double.infinity,
               height: 50,
               //color: Colors.blue,
-              child: Text("12 محصول",style: TextStyle(
+              child: const Text("12 محصول",style: TextStyle(
                   color: Colors.grey
               ),),
             ),
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.only(bottom: 3),
+                  margin: const EdgeInsets.only(bottom: 3),
                   width: 203,
                   height: 300,
                   color: Colors.white,
@@ -71,54 +69,7 @@ class _AllPageState extends State<AllPage> {
                         child: Container(
                           width: 100,
                           height: 100,
-                          margin: EdgeInsets.only(top: 50),
-                          // color: Colors.orange,
-                          child: Image.asset(
-                            width: 20,
-                            'assets/images/p11.png',
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: EdgeInsets.only(left: 120),
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.orange.shade100,
-                        ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.orange.shade900,
-                          size: 18,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text("15/000 تومان",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold
-                        ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
-                        style: TextStyle(
-                            color: Colors.grey),),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 2,),
-                Container(
-                  margin: EdgeInsets.only(right: 3, bottom: 3),
-                  width: 203,
-                  height: 300,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          margin: EdgeInsets.only(top: 50),
+                          margin: const EdgeInsets.only(top: 50),
                           //  color: Colors.orange,
                           child: Image.asset(
                             width: 20,
@@ -126,17 +77,50 @@ class _AllPageState extends State<AllPage> {
                           ),
                         ),
                       ),
-                      // SizedBox(height: 20),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 100),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
                         child: ProviderWidget(),
                       ),
-                      Text("15/000 تومان",
+                      const Text("15/000 تومان",
                         style: TextStyle(
                             fontWeight: FontWeight.bold
                         ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
+                        style: TextStyle(
+                            color: Colors.grey),),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 3, bottom: 3),
+                  width: 203,
+                  height: 300,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.only(top: 50),
+                          //  color: Colors.orange,
+                          child: Image.asset(
+                            width: 20,
+                            'assets/images/p11.png',
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
+                        child: ProviderWidget(),
+                      ),
+                      const Text("15/000 تومان",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),),
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
                         style: TextStyle(
                             color: Colors.grey),),
                     ],
@@ -147,7 +131,7 @@ class _AllPageState extends State<AllPage> {
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.only(bottom: 3),
+                  margin: const EdgeInsets.only(bottom: 3),
                   width: 203,
                   height: 300,
                   color: Colors.white,
@@ -157,54 +141,7 @@ class _AllPageState extends State<AllPage> {
                         child: Container(
                           width: 100,
                           height: 100,
-                          margin: EdgeInsets.only(top: 50),
-                          // color: Colors.orange,
-                          child: Image.asset(
-                            width: 20,
-                            'assets/images/p11.png',
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: EdgeInsets.only(left: 120),
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.orange.shade100,
-                        ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.orange.shade900,
-                          size: 18,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text("15/000 تومان",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold
-                        ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
-                        style: TextStyle(
-                            color: Colors.grey),),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 2,),
-                Container(
-                  margin: EdgeInsets.only(right: 3, bottom: 3),
-                  width: 203,
-                  height: 300,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          margin: EdgeInsets.only(top: 50),
+                          margin: const EdgeInsets.only(top: 50),
                           //  color: Colors.orange,
                           child: Image.asset(
                             width: 20,
@@ -212,28 +149,50 @@ class _AllPageState extends State<AllPage> {
                           ),
                         ),
                       ),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: EdgeInsets.only(left: 120),
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.orange.shade100,
-                        ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.orange.shade900,
-                          size: 18,
-                        ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
+                        child: ProviderWidget(),
                       ),
-                      SizedBox(height: 20),
-                      Text("15/000 تومان",
+                      const Text("15/000 تومان",
                         style: TextStyle(
                             fontWeight: FontWeight.bold
                         ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
+                        style: TextStyle(
+                            color: Colors.grey),),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 3, bottom: 3),
+                  width: 203,
+                  height: 300,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.only(top: 50),
+                          //  color: Colors.orange,
+                          child: Image.asset(
+                            width: 20,
+                            'assets/images/p11.png',
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
+                        child: ProviderWidget(),
+                      ),
+                      const Text("15/000 تومان",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),),
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
                         style: TextStyle(
                             color: Colors.grey),),
                     ],
@@ -244,7 +203,7 @@ class _AllPageState extends State<AllPage> {
             Row(
               children: [
                 Container(
-                  margin: EdgeInsets.only(bottom: 3),
+                  margin: const EdgeInsets.only(bottom: 3),
                   width: 203,
                   height: 300,
                   color: Colors.white,
@@ -254,54 +213,7 @@ class _AllPageState extends State<AllPage> {
                         child: Container(
                           width: 100,
                           height: 100,
-                          margin: EdgeInsets.only(top: 50),
-                          // color: Colors.orange,
-                          child: Image.asset(
-                            width: 20,
-                            'assets/images/p11.png',
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: EdgeInsets.only(left: 120),
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.orange.shade100,
-                        ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.orange.shade900,
-                          size: 18,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text("15/000 تومان",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold
-                        ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
-                        style: TextStyle(
-                            color: Colors.grey),),
-                    ],
-                  ),
-                ),
-                SizedBox(width: 2,),
-                Container(
-                  margin: EdgeInsets.only(right: 3, bottom: 3),
-                  width: 203,
-                  height: 300,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Center(
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          margin: EdgeInsets.only(top: 50),
+                          margin: const EdgeInsets.only(top: 50),
                           //  color: Colors.orange,
                           child: Image.asset(
                             width: 20,
@@ -309,28 +221,50 @@ class _AllPageState extends State<AllPage> {
                           ),
                         ),
                       ),
-                      SizedBox(height: 20),
-                      Container(
-                        margin: EdgeInsets.only(left: 120),
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.orange.shade100,
-                        ),
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.orange.shade900,
-                          size: 18,
-                        ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
+                        child: ProviderWidget(),
                       ),
-                      SizedBox(height: 20),
-                      Text("15/000 تومان",
+                      const Text("15/000 تومان",
                         style: TextStyle(
                             fontWeight: FontWeight.bold
                         ),),
-                      SizedBox(height: 20),
-                      Text("روغن زیتون درجه یک _ 100 گرم",
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
+                        style: TextStyle(
+                            color: Colors.grey),),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 3, bottom: 3),
+                  width: 203,
+                  height: 300,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          margin: const EdgeInsets.only(top: 50),
+                          //  color: Colors.orange,
+                          child: Image.asset(
+                            width: 20,
+                            'assets/images/p11.png',
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 100),
+                        child: ProviderWidget(),
+                      ),
+                      const Text("15/000 تومان",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),),
+                      const SizedBox(height: 20),
+                      const Text("روغن زیتون درجه یک _ 100 گرم",
                         style: TextStyle(
                             color: Colors.grey),),
                     ],
