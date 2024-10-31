220bd20dc59f1805cb0721ed5dad8596211f40dc
mehdi <mehdidehghani556@yahoo.com>
2024-10-31 21:31:39 +0330

    Edit login page

--- lib/screens/login/log_in.dart
+++ lib/screens/login/log_in.dart
@@ -52,38 +52,147 @@ class _LogInState extends State<LogIn> with SingleTickerProviderStateMixin {
     );
   }
 
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40),
-                      bottomRight: Radius.circular(40),
-                    ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
                   ),
-                  height: 360,
                 ),
-                Container(
-                  decoration: BoxDecoration(color: Colors.red,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40),
-                      topRight: Radius.circular(40),
+                height: 350,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextField(
+                            controller: usernameController,
+                            decoration: InputDecoration(
+                              labelText: 'نام کاربری',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                // گوشه‌های گرد
+                                borderSide: BorderSide(
+                                    color: Colors.blue,
+                                    width: 2.0), // رنگ و عرض خط
+                              ),
+                              filled: true,
+                              // فعال‌سازی پس‌زمینه
+                              fillColor: Colors.white,
+                              // رنگ پس‌زمینه
+                              hintStyle: TextStyle(color: Colors.grey),
+                              // رنگ متن راهنما
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 15.0,
+                                  horizontal: 10.0), // فاصله داخلی
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          TextField(
+                            controller: usernameController,
+                            decoration: InputDecoration(
+                              labelText: 'رمز عبور',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                // گوشه‌های گرد
+                                borderSide: BorderSide(
+                                    color: Colors.blue,
+                                    width: 2.0), // رنگ و عرض خط
+                              ),
+                              filled: true,
+                              // فعال‌سازی پس‌زمینه
+                              fillColor: Colors.white,
+                              // رنگ پس‌زمینه
+                              hintStyle: TextStyle(color: Colors.grey),
+                              // رنگ متن راهنما
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 15.0,
+                                  horizontal: 10.0), // فاصله داخلی
+                            ),
+                          ),
+                          SizedBox(height: 36.0),
+                          Container(
+                            width: double.infinity, // عرض کامل
+                            height: 50.0, // ارتفاع دکمه
+                            decoration: BoxDecoration(
+                              color: Colors.black12, // رنگ پس‌زمینه
+                              borderRadius:
+                                  BorderRadius.circular(10.0), // گوشه‌های گرد
+                            ),
+                            child: TextButton(
+                              onPressed: () {
+                                // Logic for login
+                                String username = usernameController.text;
+                                String password = passwordController.text;
+                                // Add your login logic here
+                              },
+                              child: InkWell(
+                                onTap: () {
+                                  NextPage(context);
+                                },
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white, // رنگ متن
+                                    fontSize: 18.0, // اندازه متن
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "حساب کاربری ندارید؟",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              InkWell(
+                                  onTap: () {
+                                    signIn(context);
+                                  },
+                                  child: Text("ثبت نام")),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  height: 350,
+                  ],
                 ),
-              ],
-            ),
-            Padding(padding: EdgeInsets.all(100),child: Image.asset("assets/images/login2.png")),
-          ],
-        ),
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                height: 357,
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(40),
+            child: Image.asset("assets/images/login3.png"),
+          ),
+        ],
       ),
     );
   }
