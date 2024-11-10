2ba3e918d85451b447fa254270f40b531aeb7210
mehdi <mehdidehghani556@yahoo.com>
2024-11-10 21:02:45 +0330

    Edit product card for get info instance category Id
    and display at home Body

--- lib/product/product_Card_Widget/product_Card_List_Widget.dart
+++ lib/product/product_Card_Widget/product_Card_List_Widget.dart
@@ -5,7 +5,14 @@ import 'package:abd_shop/product/product_Card_Widget/product_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProductCardListWidget extends StatefulWidget {
-  const ProductCardListWidget({super.key});
+  const ProductCardListWidget({
+    super.key,
+    required this.categoryId,
+    required this.categoryName,
+  });
+
+  final int categoryId;
+  final String categoryName;
 
   @override
   State<ProductCardListWidget> createState() => _ProductCardListWidgetState();
@@ -15,25 +22,23 @@ class _ProductCardListWidgetState extends State<ProductCardListWidget> {
   List<ProductCard> productWidgetList = [];
 
   initMarkets() async {
-    await getProducts().then((value) {
-      DataResponse dataResponse = value;
+    try {
+      DataResponse dataResponse = await getProductsOfCategory(widget.categoryId);
       if (dataResponse.status == 1) {
-        for (var item in dataResponse.data) {
-          Product productModel = Product.fromJson(item);
-          productWidgetList.add(
-            ProductCard(
-              productModel: productModel,
-            ),
-          );
-        }
+        setState(() {
+          productWidgetList = dataResponse.data.map<ProductCard>((item) {
+            Product productModel = Product.fromJson(item);
+            return ProductCard(productModel: productModel);
+          }).toList();
+        });
       } else {
         print('run another status');
       }
-    }).catchError((onError) {
+    } catch (onError) {
       throw Exception(
-          'Exception error: markets_list_widget.dart in getDataFromServer: $onError');
-    });
-    setState(() {});
+        'Exception error: markets_list_widget.dart in getDataFromServer: $onError',
+      );
+    }
   }
 
   @override
@@ -44,10 +49,50 @@ class _ProductCardListWidgetState extends State<ProductCardListWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(scrollDirection: Axis.horizontal,
-      child: Row(
-        children: productWidgetList,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(height: 60,width: 69,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepOrange, Colors.orange], // Gradient background
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 8,
+                offset: Offset(4, 4),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(16.0),
+          margin: const EdgeInsets.all(10.0),
+          child: Text(
+            widget.categoryName,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              shadows: [
+                Shadow(
+                  blurRadius: 5.0,
+                  color: Colors.black54,
+                  offset: Offset(2.0, 2.0),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: productWidgetList,
+          ),
+        ),
+      ],
     );
   }
 }
