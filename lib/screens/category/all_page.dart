1f9c499c2b2797923266eb4a67ca1d3f7ed302a3
Ali Nti <Ali3nti@gmail.com>
2024-12-08 01:28:24 +0330

    add shimmer in allPage and edit page

--- lib/screens/category/all_page.dart
+++ lib/screens/category/all_page.dart
@@ -4,7 +4,10 @@ import 'package:abd_shop/models/response_model.dart';
 import 'package:abd_shop/services/api_helper.dart';
 import 'package:abd_shop/widget/my_app_bar.dart';
 import 'package:abd_shop/widget/provider/add_to_cart_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class AllPage extends StatefulWidget {
   const AllPage({
@@ -17,8 +20,8 @@ class AllPage extends StatefulWidget {
 
 class _AllPageState extends State<AllPage> {
   List<Product> productList = [];
-  initProductsList() {
-    getProducts().then((value) {
+  initProductsList() async {
+    await getProducts().then((value) {
       DataResponse response = value;
       if (response.status == 1) {
         List<dynamic> list = response.data;
@@ -39,11 +42,11 @@ class _AllPageState extends State<AllPage> {
   @override
   void initState() {
     super.initState();
-    initProductsList();
   }
 
   @override
   Widget build(BuildContext context) {
+    initProductsList();
     return Scaffold(
       backgroundColor: Colors.grey.shade50,
       appBar: const MyAppBar(),
@@ -64,93 +67,167 @@ class _AllPageState extends State<AllPage> {
                     size: 18,
                   ),
                   const SizedBox(width: 20),
-                  Container(
-                    width: 100,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.grey.shade200,
-                    ),
-                    child: const Center(
-                      child: Text("همه محصولات"),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {});
+                    },
+                    child: Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: const Center(
+                        child: Text("همه محصولات"),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-            Container(
-              padding: const EdgeInsets.only(top: 10, right: 20),
-              margin: const EdgeInsets.only(top: 20),
-              width: double.infinity,
-              height: 50,
-              //color: Colors.blue,
-              child: const Text(
-                productList.length.toString(),
-                style: TextStyle(color: Colors.grey),
-              ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 500,
-              child: GridView(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                children: productList
-                    .map(
-                      (item) => Container(
-                        margin: const EdgeInsets.only(bottom: 3),
-                        width: 303,
-                        height: 400,
-                        color: Colors.white,
-                        child: Column(
-                          children: [
-                            Center(
-                              child: Container(
-                                // width: 100,
-                                // height: 100,
-                                margin: const EdgeInsets.only(top: 5),
-                                //  color: Colors.orange,
-                                child: Image.network(
-                                  baseUrl + item.image,
-                                  height: 80,
-                                  width: 100,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 100),
-                              child: AddToCartWidget(
-                                product: Product(),
-                              ),
-                            ),
-                            Text(
-                              item.price.toString(),
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(height: 20),
-                            Row(
-                              children: [
-                                Text(
-                                  item.name,
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                                SizedBox(width: 2),
-                                Text(
-                                  item.discount.toString(),
-                                ),
-                              ],
-                            ),
-                          ],
+            (productList.isNotEmpty)
+                ? Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, right: 20),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        height: 50,
+                        //color: Colors.blue,
+                        child: Text(
+                          "${productList.length} محصول",
+                          style: const TextStyle(color: Colors.grey),
                         ),
                       ),
-                    )
-                    .toList(),
-              ),
-            ),
+                      GridView(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                        ),
+                        shrinkWrap: true,
+                        children: productList
+                            .map(
+                              (item) => Container(
+                                margin: const EdgeInsets.only(bottom: 3),
+                                width: 303,
+                                height: 400,
+                                color: Colors.white,
+                                child: Column(
+                                  children: [
+                                    Center(
+                                      child: Container(
+                                        // width: 100,
+                                        // height: 100,
+                                        margin: const EdgeInsets.only(top: 5),
+                                        //  color: Colors.orange,
+                                        child: Image.network(
+                                          baseUrl + item.image,
+                                          height: 80,
+                                          width: 100,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 100),
+                                      child: AddToCartWidget(
+                                        product: Product(),
+                                      ),
+                                    ),
+                                    Text(
+                                      item.price.toString(),
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          item.name,
+                                          style: const TextStyle(
+                                              color: Colors.grey),
+                                        ),
+                                        const SizedBox(width: 2),
+                                        Text(
+                                          item.discount.toString(),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ],
+                  )
+                : _shimmerList(),
           ],
         ),
       ),
     );
+  }
+
+  Widget _shimmerList() {
+    try {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemCount: 4,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.only(bottom: 3),
+              color: Colors.white,
+              child: Shimmer.fromColors(
+                baseColor: Colors.grey.shade300,
+                highlightColor: Colors.grey.shade100,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        height: 80,
+                        width: 100,
+                        color: Colors.grey.shade200,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 20,
+                      width: 50,
+                      color: Colors.grey.shade200,
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 80,
+                          color: Colors.grey.shade200,
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          height: 30,
+                          width: 20,
+                          color: Colors.grey.shade200,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    } catch (e) {
+      if (kDebugMode) {
+        print("Exception - wallet_stations_page.dart - _shimmerList():$e");
+      }
+      return const SizedBox();
+    }
   }
 }
