01cccb27204fce65dba731dbd64e2fee72611151
mehdi <mehdidehghani556@yahoo.com>
2024-12-11 10:42:53 +0330

    Edit Size & add new widget

--- lib/screens/orders/Order_Tracking_Page.dart
+++ lib/screens/orders/Order_Tracking_Page.dart
@@ -1,3 +1,4 @@
+import 'package:abd_shop/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -38,12 +39,12 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
     String timerText =
         "${remainingTime.inMinutes.remainder(60)}:${(remainingTime.inSeconds.remainder(60)).toString().padLeft(2, '0')}";
 
-    // محاسبه درصد باقی‌مانده
     double progress = remainingTime.inSeconds / (30 * 60);
 
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: EdgeInsets.only(
@@ -70,13 +71,13 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
                 color: Colors.greenAccent.shade100,
                 borderRadius: BorderRadiusDirectional.circular(10),
               ),
-              width: 380,
+              width: 480,
               height: 100,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
                         "سفارش تحویل داده شد",
@@ -87,7 +88,9 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
                       Row(
                         children: [
                           Text("تحویل تا"),
-                          SizedBox(width: 5,),
+                          SizedBox(
+                            width: 5,
+                          ),
                           Text(
                             timerText,
                             style: TextStyle(
@@ -107,6 +110,118 @@ class _OrderTrackingPageState extends State<OrderTrackingPage> {
                 ],
               ),
             ),
+            Text(
+              "ارسال به خانه",
+              style: TextStyle(fontWeight: FontWeight.normal),
+            ),
+            Text(
+              "میدان آزادی،خیابان تلاش،کوچه هفتم",
+              style: TextStyle(fontWeight: FontWeight.w900),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "فروشگاه",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Colors.grey),
+                ),
+                Text(
+                  "دیلی مارکت /بلوار نصر >",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Colors.blue),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "زمان ثبت سفارش",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Colors.grey),
+                ),
+                Text(
+                  "جمعه،18آبان1403 19:42",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "شناسه پیگیری",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Colors.grey),
+                ),
+                Text(
+                  "190-118687-386",
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 6, left: 6, top: 6),
+                  child: Container(
+                    height: 52,
+                    decoration: BoxDecoration(
+                      color: Colors.deepOrange,
+                      borderRadius: BorderRadiusDirectional.circular(10),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 7, left: 7, top: 7),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: CupertinoColors.white,
+                        borderRadius: BorderRadiusDirectional.circular(10)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.sms_outlined, color: Colors.deepOrange),
+                        SizedBox(width: 10),
+                        Text(
+                          "ثبت امتیاز و بازخورد",
+                          style:
+                              TextStyle(color: Colors.deepOrange, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ), // Container for rate & comments
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 4,
+              color: Colors.grey.shade300,
+            ),
+            Text("سبد خرید",style: TextStyle(fontWeight: FontWeight.w900),),
           ],
         ),
       ),
