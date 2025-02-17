ce483570c16bee755820658afa2851532875c560
mehdi <mehdidehghani556@yahoo.com>
2025-02-17 22:57:38 +0330

    delete wallet page

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -18,7 +18,6 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-
 void address(BuildContext context) {
   Navigator.push(
     context,
@@ -149,30 +148,23 @@ class _ProfilePageState extends State<ProfilePage> {
             children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: InkWell(
-                  onTap: () {
-                    walletPage(context);
-                  },
+                child: GestureDetector(
+                  onTap: () => address(context),
                   child: Row(
                     children: [
-                      Image.asset("assets/images/wallet.png",
+                      Image.asset("assets/images/mapp.png",
                           width: 50, height: 50),
-                      const SizedBox(width: 10),
+                      SizedBox(width: 10),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Text(
-                            "کیف پول",
+                          Text(
+                            "آدرس ها",
                             style: TextStyle(fontSize: 18),
                           ),
-                          Row(
-                            children: [
-                              const Text(
-                                "10000",
-                                style: TextStyle(fontSize: 16),
-                              ),
-                              Image.asset("assets/images/toman.png", height: 15),
-                            ],
+                          Text(
+                            "2 آدرس",
+                            style: TextStyle(fontSize: 16,color: Colors.grey),
                           ),
                         ],
                       ),
@@ -193,12 +185,6 @@ class _ProfilePageState extends State<ProfilePage> {
           //   leading: Image.asset("assets/images/send-2.png"),
           //   onTap: () => invite(context),
           // ),
-          ListTile(
-            title: const Text("آدرس ها"),
-            leading: Image.asset("assets/images/location.png"),
-            onTap: () => address(context),
-          ),
-          const Divider(),
           ListTile(
             title: const Text("فروشندگان"),
             leading: Image.asset("assets/images/shop.png"),
